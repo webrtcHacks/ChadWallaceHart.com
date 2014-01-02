301e604180e022706476b0a17cb07c953990efa5
webrtcHacks <webrtchacks@gmail.com>
2014-01-01 21:14:47 -0500

    First commit

---
+++ easyrtc/js/demo_audio_video_simple.js
@@ -0,0 +1,53 @@
+var selfEasyrtcid = "";
+
+
+function connect() {
+    easyrtc.setRoomOccupantListener(convertListToButtons);
+    easyrtc.easyApp("easyrtc.audioVideo", "selfVideo", ["callerVideo"], loginSuccess, loginFailure);
+ }
+
+
+function clearConnectList() {
+    var otherClientDiv = document.getElementById('otherClients');
+    while (otherClientDiv.hasChildNodes()) {
+        otherClientDiv.removeChild(otherClientDiv.lastChild);
+    }
+}
+
+
+function convertListToButtons (roomName, data, isPrimary) {
+    clearConnectList();
+    var otherClientDiv = document.getElementById('otherClients');
+    for(var easyrtcid in data) {
+        var button = document.createElement('button');
+        button.onclick = function(easyrtcid) {
+            return function() {
+                performCall(easyrtcid);
+            };
+        }(easyrtcid);
+
+        var label = document.createTextNode(easyrtc.idToName(easyrtcid));
+        button.appendChild(label);
+        otherClientDiv.appendChild(button);
+    }
+}
+
+
+function performCall(otherEasyrtcid) {
+    easyrtc.hangupAll();
+
+    var successCB = function() {};
+    var failureCB = function() {};
+    easyrtc.call(otherEasyrtcid, successCB, failureCB);
+}
+
+
+function loginSuccess(easyrtcId) {
+    selfEasyrtcid = easyrtcId;
+    document.getElementById("iam").innerHTML = "I am " + easyrtc.cleanId(easyrtcId);
+}
+
+
+function loginFailure(errorCode, message) {
+    easyrtc.showError(errorCode, message);
+}
---
+++ server.js
@@ -0,0 +1,158 @@
+//Modules
+var express = require('express');
+var app = express();
+var io      = require("socket.io");         // web socket external module
+var easyrtc = require("easyrtc");           // EasyRTC external module
+
+//Statically serve files in these directories  -easyRTC
+app.use("/js", express.static(__dirname + '/easyrtc/js'));
+app.use("/images", express.static(__dirname + '/easyrtc/images'));
+app.use("/css", express.static(__dirname + '/easyrtc/css'));
+
+//For my homepage
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.use("/css", express.static(__dirname + '/public/css'));
+app.use("/img", express.static(__dirname + '/public/img'));
+
+// Needed to parse form data 
+app.use(express.bodyParser());
+
+//global variables
+var loggedIn = false,
+    password = 'test';
+
+//Temporary home page
+app.get('/', function (req, res) {
+    if (loggedIn == true) {
+        res.render('index', { title: "Chad is available to WebRTC" });
+        console.log("homepage -logged in " + loggedIn);
+    }
+    else {
+        res.render('index', { title: "Chad is NOT available to WebRTC" });
+        console.log("homepage -not logged in " + loggedIn);
+    }
+
+    //console.log(process.cwd());
+});
+
+//Serve a static login page if not logged in already
+app.get('/login', function (req, res) {
+    console.log('Login attempt');
+    if (loggedIn == true) {
+        res.sendfile(__dirname + '/easyrtc/demo_multiparty.html');
+    }
+    else {
+        res.sendfile(__dirname + '/public/login.html');
+    }
+});
+
+//Respond to POST from login form
+app.post('/login', function (req, res) {
+    if (loggedIn == true) {
+        res.send("Already logged in.");
+    }
+    else {
+        console.log("Posted data:" + JSON.stringify(req.body));
+        if (req.body.pw == password) {
+            loggedIn = true;
+            res.send("logged in");
+            console.log("Logged in");
+
+            // Start EasyRTC server
+            var easyrtcServer = easyrtc.listen(app, socketServer, {'apiEnable':'true'});
+
+        }
+        else { res.send("Incorrect password.") }
+    }
+});
+
+//Serve a static logout page
+app.get('/logout', function (req, res) {
+    if (loggedIn == true) {
+        res.sendfile(__dirname + '/public/logout.html');
+        console.log("logout page");
+
+    }
+    else {
+        res.send("Not logged in.")
+    }
+});
+
+//Check the password to prevent unauthoried logouts
+app.post('/logout', function (req, res) {
+    console.log("Posted data:" + JSON.stringify(req.body));
+    if (req.body.pw == password) {
+        if (loggedIn == true) {
+            loggedIn = false;
+            res.send("Logged out");
+            console.log("logged out");
+
+            //Consider killing all active sessions here
+            easyrtc.setOption('apiEnable', 'false');
+        }
+        else {
+            res.send("You were already logged out");
+            console.log("Attempt to logout when not logged in");
+        }
+    }
+    else {
+        console.log("Bad password attempt");
+        res.send("Incorrect password");
+    }
+})
+
+//Initiate a call
+app.get('/call', function(req, res){
+    if (loggedIn == true) {
+        res.sendfile(__dirname + '/easyrtc/demo_multiparty.html');
+    }
+    else {
+        res.send("Chad is not available. Please try later.")
+    }
+});
+
+//var webServer = app.listen(process.env.port || 8080);
+var webServer = app.listen(process.env.port || 8080);
+console.log('Listening on port ' + process.env.port || 8080);
+
+// Start Socket.io so it attaches itself to Express server
+var socketServer = io.listen(webServer);
+
+// Configuration
+/*
+app.configure(function() {
+    app.use(express.static(__dirname + "/public/"));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+*/
+//failed kill session attempts
+/*
+//did nothing
+//http://stackoverflow.com/questions/16000120/socket-io-close-server
+var socketlist = [];
+socketServer.sockets.on('connection', function(socket) {
+    socketlist.push(socket);
+    socket.emit('socket_is_connected','You are connected!');
+    socket.on('close', function () {
+      console.log('socket closed');
+      socketlist.splice(socketlist.indexOf(socket), 1);
+    });
+});
+*/
+
+/* //didn't work
+               // shut down the WS server
+               socketServer.close();
+               
+               //kill open sockets
+               socketlist.forEach(function(socket) {socket.destroy()});*/
